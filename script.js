2a3b472e6fa7f032f26e9b19ecb81d8a3cbf377c
Fernando Jimenez <fgjv06@gmail.com>
2023-03-04 19:52:44 -0300

    JS - tabs/tabContents[index].classList.add(active)

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ tabs.forEach((tab,index) => {
   tab.addEventListener("click", () => {
     tabContents.forEach((content) => {
       content.classList.remove("active");
-    })
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
   })
 })
